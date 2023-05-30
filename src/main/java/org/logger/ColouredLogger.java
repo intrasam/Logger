45116756f1ddf0d2e0ef9309cf4fe1d4bec71877
54m3l3 <guccionesamuele@gmail.com>
2023-05-30 17:10:47 +0200

    refactor(Logger): Bug fix on logging

--- src/main/java/org/logger/ColouredLogger.java
+++ src/main/java/org/logger/ColouredLogger.java
@@ -1,8 +1,10 @@
 package org.logger;
 
+import lombok.extern.slf4j.Slf4j;
 import org.logger.interfaces.Log;
 
 @Log
+@Slf4j
 public class ColouredLogger {
 	public int method1(){
 		int x=10;
--- src/main/java/org/logger/LoggingAspect.java
+++ src/main/java/org/logger/LoggingAspect.java
@@ -1,8 +1,14 @@
 package org.logger;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.jetbrains.annotations.NotNull;
 import org.logger.interfaces.Logger;
+
+import java.lang.reflect.Method;
 
 
 @Aspect
@@ -15,21 +21,46 @@ public class LoggingAspect {
 
 	@Before("logMethodExecution()")
 	public void logMethodEntry(JoinPoint joinPoint) {
-		String className = joinPoint.getSignature().getDeclaringTypeName();
-		String methodName = joinPoint.getSignature().getName();
-		logger.log(LogLevel.INFO, "Entering method: "  + className + "." + methodName, null);
+		if (isAlreadyLogged(joinPoint)) {
+			String className = joinPoint.getSignature().getDeclaringTypeName();
+			String methodName = joinPoint.getSignature().getName();
+			logger.log(LogLevel.INFO, "Entering method: " + className + "." + methodName, null);
+		}
 	}
 	@AfterReturning(pointcut="logMethodExecution()", returning="returnValue")
 	public void logMethodExit(JoinPoint joinPoint, Object returnValue) {
-		String className = joinPoint.getSignature().getDeclaringTypeName();
-		String methodName = joinPoint.getSignature().getName();
-		logger.log(LogLevel.INFO, "Exiting method: " + className + "." + methodName + " with return value: " + returnValue, null);
+		if (isAlreadyLogged(joinPoint)) {
+
+			String className = joinPoint.getSignature().getDeclaringTypeName();
+			String methodName = joinPoint.getSignature().getName();
+			logger.log(LogLevel.INFO, "Exiting method: " + className + "." + methodName + " with return value: " + returnValue, null);
+		}
 	}
 	@AfterThrowing(pointcut="logMethodExecution()", throwing="exception")
 	public void logMethodException(JoinPoint joinPoint, Throwable exception) {
-		String className = joinPoint.getSignature().getDeclaringTypeName();
-		String methodName = joinPoint.getSignature().getName();
-		logger.log(LogLevel.FATAL, "Exception thrown in method: " + className + "." + methodName, exception);
+		if (isAlreadyLogged(joinPoint)) {
+
+			String className = joinPoint.getSignature().getDeclaringTypeName();
+			String methodName = joinPoint.getSignature().getName();
+			logger.log(LogLevel.FATAL, "Exception thrown in method: " + className + "." + methodName, exception);
+		}
+	}
+	private boolean isAlreadyLogged(@NotNull JoinPoint joinPoint) {
+		// Check for the presence of logging annotations on the class
+		Class<?> targetClass = joinPoint.getClass();
+		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+		Method targetMethod = methodSignature.getMethod();
+		if (targetClass.isAnnotationPresent(Slf4j.class) ||
+				targetClass.isAnnotationPresent(Log4j.class)){
+				//targetClass.isAnnotationPresent(Loggable.class)) {
+			return false;
+		}
+
+		// Check for the presence of logging annotations on the method
+
+		//targetMethod.isAnnotationPresent(Loggable.class)) {
+		return !targetMethod.isAnnotationPresent(Slf4j.class) &&
+				!targetMethod.isAnnotationPresent(Log4j.class);
 	}
 
 }
